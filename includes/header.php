394020e69bbc3106406884450b08f82b4b4be5f1
kimjayson-ph <kimjayson.ph@gmail.com>
2023-05-27 02:42:27 +0800

    fix show only user icon when login

--- includes/header.php
+++ includes/header.php
@@ -31,32 +31,34 @@
             <div class="social-follow">
               <div class="user_login" style="background-color: #7bc41d;">
                 <ul>
-                  <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i>
-                      <?php
-                      $email = $_SESSION['login'];
-                      $sql = "SELECT FullName FROM tblusers WHERE EmailId=:email ";
-                      $query = $dbh->prepare($sql);
-                      $query->bindParam(':email', $email, PDO::PARAM_STR);
-                      $query->execute();
-                      $results = $query->fetchAll(PDO::FETCH_OBJ);
-                      if ($query->rowCount() > 0) {
-                        foreach ($results as $result) {
+                  <?php if ($_SESSION['login']) { ?>
+                    <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i>
+                        <?php
+                        $email = $_SESSION['login'];
+                        $sql = "SELECT FullName FROM tblusers WHERE EmailId=:email ";
+                        $query = $dbh->prepare($sql);
+                        $query->bindParam(':email', $email, PDO::PARAM_STR);
+                        $query->execute();
+                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                        if ($query->rowCount() > 0) {
+                          foreach ($results as $result) {
 
-                          echo htmlentities($result->FullName);
-                        }
-                      } ?>
-                      <i class="fa fa-angle-down" aria-hidden="true"></i></a>
-                    <ul class="dropdown-menu" style="background-color: #7bc41d;">
-                      <?php if ($_SESSION['login']) { ?>
-                        <li><a href="profile.php">Profile Settings</a></li>
-                        <li><a href="update-password.php">Update Password</a></li>
-                        <li><a href="my-booking.php">My Booking</a></li>
-                        <li><a href="post-testimonial.php">Post a Testimonial</a></li>
-                        <li><a href="my-testimonials.php">My Testimonial</a></li>
-                        <li><a href="logout.php">Sign Out</a></li>
-                      <?php } ?>
-                    </ul>
-                  </li>
+                            echo htmlentities($result->FullName);
+                          }
+                        } ?>
+                        <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                      <ul class="dropdown-menu" style="background-color: #7bc41d;">
+                        <?php if ($_SESSION['login']) { ?>
+                          <li><a href="profile.php">Profile Settings</a></li>
+                          <li><a href="update-password.php">Update Password</a></li>
+                          <li><a href="my-booking.php">My Booking</a></li>
+                          <li><a href="post-testimonial.php">Post a Testimonial</a></li>
+                          <li><a href="my-testimonials.php">My Testimonial</a></li>
+                          <li><a href="logout.php">Sign Out</a></li>
+                        <?php } ?>
+                      </ul>
+                    </li>
+                  <?php    } ?>
                 </ul>
               </div>
             </div>
