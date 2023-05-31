6d144146da68785442748a0468c54538d29687cf
genereodan22 <125712253+genereodan22@users.noreply.github.com>
2023-05-31 16:29:24 +0800

    modified:   admin/manage-pages.php

--- admin/manage-pages.php
+++ admin/manage-pages.php
@@ -119,12 +119,9 @@ function MM_jumpMenu(targ,selObj,restore){ //v3.0
 			<?php include('includes/leftbar.php'); ?>
 			<div class="content-wrapper">
 				<div class="container-fluid">
-
 					<div class="row">
 						<div class="col-md-12">
-
 							<h2 class="page-title">Manage Pages </h2>
-
 							<div class="row">
 								<div class="col-md-10">
 									<div class="panel panel-default">
@@ -180,25 +177,24 @@ function MM_jumpMenu(targ,selObj,restore){ //v3.0
 												</div>
 
 												<div class="form-group">
-													<label class="col-sm-4 control-label">Page Details </label>
+														<label class="col-sm-4 control-label">Page Details </label>
 													<div class="col-sm-8">
 														<textarea class="form-control" rows="5" cols="50" name="pgedetails" id="pgedetails" placeholder="Package Details" required>
-										<?php
-										$pagetype = $_GET['type'];
-										$sql = "SELECT detail from tblpages where type=:pagetype";
-										$query = $dbh->prepare($sql);
-										$query->bindParam(':pagetype', $pagetype, PDO::PARAM_STR);
-										$query->execute();
-										$results = $query->fetchAll(PDO::FETCH_OBJ);
-										$cnt = 1;
-										if ($query->rowCount() > 0) {
-											foreach ($results as $result) {
-												echo htmlentities($result->detail);
-											}
-										}
-										?>
-
-										</textarea>
+														<?php
+														$pagetype = $_GET['type'];
+														$sql = "SELECT detail from tblpages where type=:pagetype";
+														$query = $dbh->prepare($sql);
+														$query->bindParam(':pagetype', $pagetype, PDO::PARAM_STR);
+														$query->execute();
+														$results = $query->fetchAll(PDO::FETCH_OBJ);
+														$cnt = 1;
+														if ($query->rowCount() > 0) {
+															foreach ($results as $result) {
+																echo htmlentities($result->detail);
+															}
+														}
+														?>
+														</textarea>
 													</div>
 												</div>
 
@@ -208,21 +204,13 @@ function MM_jumpMenu(targ,selObj,restore){ //v3.0
 														<button type="submit" name="submit" value="Update" id="submit" class="btn-primary btn">Update</button>
 													</div>
 												</div>
-
 											</form>
-
 										</div>
 									</div>
 								</div>
-
 							</div>
-
-
-
 						</div>
 					</div>
-
-
 				</div>
 			</div>
 		</div>
