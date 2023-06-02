166462213f5877397ae887f0638dddb9e4c4eb78
genereodan22 <125712253+genereodan22@users.noreply.github.com>
2023-06-02 16:49:03 +0800

    modified:   includes/footer.php 	modified:   index.php

--- index.php
+++ index.php
@@ -218,6 +218,25 @@ error_reporting(0);
     <div class="dark-overlay"></div>
   </section>
   <!-- /Testimonial-->
+  
+  <script>
+  window.addEventListener('mouseover', initLandbot, { once: true });
+  window.addEventListener('touchstart', initLandbot, { once: true });
+  var myLandbot;
+  function initLandbot() {
+    if (!myLandbot) {
+      var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+      s.addEventListener('load', function() {
+        myLandbot = new Landbot.Popup({
+          configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-1617561-DJ8S7ZNEIFJAHEEL/index.json',
+        });
+      });
+      s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    }
+  }
+  </script>
 
 
   <!--Footer -->
@@ -240,6 +259,9 @@ error_reporting(0);
   <!--Forgot-password-Form -->
   <?php include('includes/forgotpassword.php'); ?>
   <!--/Forgot-password-Form -->
+
+  
+  
 
   <!-- Scripts -->
   <script src="assets/js/jquery.min.js"></script>
